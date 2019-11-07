179e3e127a3c62d078e33b499818149894d2861f
Maksym <maksimus2998@gmail.com>
2019-11-07 01:09:11 +0100

    Change bg by time

--- app/src/main/java/com/example/flexbattle/MainActivity.java
+++ app/src/main/java/com/example/flexbattle/MainActivity.java
@@ -2,23 +2,61 @@ package com.example.flexbattle;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.WindowManager;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Calendar;
 
 public class MainActivity extends Activity {
 
+    LinearLayout linearLayout;
+    TextView greetingTextView;
+
+    @SuppressLint({"WrongViewCast", "SetTextI18n"})
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // make status bar transparent
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         setContentView(R.layout.activiti_main);
         setOnTouchListenerToMainMenuButtons();
+        setGreetingTextByTime();
+
+
     }
 
-
+    @SuppressLint("NewApi")
+    private void setGreetingTextByTime(){
+        linearLayout = findViewById(R.id.mainLayout);
+        greetingTextView = findViewById(R.id.greetingText);
+        Calendar calendar = Calendar.getInstance();
+        int timeOfDay = calendar.get(Calendar.HOUR_OF_DAY);
+        if (timeOfDay < 12) {
+            //morning
+            linearLayout.setBackground(getDrawable(R.drawable.morning_bg));
+            greetingTextView.setText("Good Morning");
+        } else if (timeOfDay >= 12 && timeOfDay < 16) {
+            //afternoon
+            greetingTextView.setText("Good Afternoon");
+        } else if (timeOfDay >= 16 && timeOfDay < 21) {
+            //evening
+            greetingTextView.setText("Good Evening");
+        } else if (timeOfDay >= 21 && timeOfDay < 24) {
+            //night
+            linearLayout.setBackground(getDrawable(R.drawable.night_bg));
+            greetingTextView.setText("Good Night");
+        }
+    }
     @SuppressLint("ClickableViewAccessibility")
     private void setOnTouchListenerToMainMenuButtons() {
         final Button soloPlayButton = findViewById(R.id.btn_soloPlay);
--- app/src/main/java/com/example/flexbattle/SoloLoginActivity.java
+++ app/src/main/java/com/example/flexbattle/SoloLoginActivity.java
@@ -2,13 +2,17 @@ package com.example.flexbattle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 
-public class SoloLoginActivity extends AppCompatActivity {
+public class SoloLoginActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_solo_login);
+
     }
 }
